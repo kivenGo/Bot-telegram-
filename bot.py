89e59f94bba17f9218d331cae5397996b9401438
kivenGo <117470367+kivenGo@users.noreply.github.com>
2022-12-15 15:12:27 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,9 @@
+import telebot
+
+bot = telebot.TeleBot("YOUR_BOT_TOKEN")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello, welcome to my bot!")
+
+bot.polling()
